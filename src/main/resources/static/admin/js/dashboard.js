4fce9bc9696b8bdbedf240f529bfda10c87e8bd5
tarzan <1334512682@qq.com>
2021-07-15 12:30:19 +0800

    删除无用代码优化

--- src/main/resources/static/admin/js/dashboard.js
+++ src/main/resources/static/admin/js/dashboard.js
@@ -27,47 +27,6 @@ $(function () {
         window.alert('你选择了: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
     })
 
-    // jvectormap data
-    var visitorsData = {
-        'US': 398, //USA
-        'SA': 400, //Saudi Arabia
-        'CA': 1000, //Canada
-        'DE': 500, //Germany
-        'FR': 760, //France
-        'CN': 300, //China
-        'AU': 700, //Australia
-        'BR': 600, //Brazil
-        'IN': 800, //India
-        'GB': 320, //Great Britain
-        'RU': 3000 //Russia
-    }
-    // World map by jvectormap
-    $('#world-map').vectorMap({
-        map: 'world_en',
-        backgroundColor: 'transparent',
-        regionStyle: {
-            initial: {
-                fill: 'rgba(255, 255, 255, 0.7)',
-                'fill-opacity': 1,
-                stroke: 'rgba(0,0,0,.2)',
-                'stroke-width': 1,
-                'stroke-opacity': 1
-            }
-        },
-        series: {
-            regions: [{
-                values: visitorsData,
-                scale: ['#ffffff', '#0154ad'],
-                normalizeFunction: 'polynomial'
-            }]
-        },
-        onRegionLabelShow: function (e, el, code) {
-            if (typeof visitorsData[code] != 'undefined')
-                el.html(el.html() + ': ' + visitorsData[code] + ' new visitors')
-        }
-    })
-
-
     /* Chart.js Charts */
     // Visit chart
     var visitChartCanvas = document.getElementById('revenue-chart-canvas').getContext('2d');

0cb91edd593ee1c0646a03e3cbbed01effeb111e
liuya <1334512682@qq.com>
2021-11-08 12:46:52 +0800

    整合第三方登录

---
+++ src/main/java/com/tarzan/cms/common/properties/SocialProperties.java
@@ -0,0 +1,20 @@
+package com.tarzan.cms.common.properties;
+
+import com.google.common.collect.Maps;
+import lombok.Data;
+import me.zhyd.oauth.config.AuthConfig;
+import me.zhyd.oauth.config.AuthDefaultSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "social")
+public class SocialProperties {
+    private Boolean enabled = false;
+    private String domain;
+    private Map<AuthDefaultSource, AuthConfig> oauth = Maps.newHashMap();
+    private Map<String, String> alias = Maps.newHashMap();
+}

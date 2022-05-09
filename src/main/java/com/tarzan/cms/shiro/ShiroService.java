23c7173cade15575c086617362cb644bf5bc57da
刘亚鹏 <1334512682@qq.com>
2022-05-09 11:32:06 +0800

    权限配置优化

--- src/main/java/com/tarzan/cms/shiro/ShiroService.java
+++ src/main/java/com/tarzan/cms/shiro/ShiroService.java
@@ -1,7 +1,10 @@
 package com.tarzan.cms.shiro;
 
+import com.tarzan.cms.common.constant.CoreConst;
 import com.tarzan.cms.common.props.FileUploadProperties;
 import com.tarzan.cms.common.props.StaticHtmlProperties;
+import com.tarzan.cms.modules.admin.model.sys.Menu;
+import com.tarzan.cms.modules.admin.service.sys.MenuService;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,20 +29,15 @@ import java.util.Map;
 @AllArgsConstructor
 public class ShiroService {
 
-   // private final MenuService MenuService;
+    private final MenuService menuService;
     private final ShiroFilterFactoryBean shiroFilterFactoryBean;
     private final FileUploadProperties fileUploadProperties;
     private final StaticHtmlProperties staticHtmlProperties;
 
-    @PostConstruct
-    public void init() {
-        updatePermission();
-    }
-
     /**
      * 初始化权限
      */
-    public Map<String, String> loadFilterChainDefinitions() {
+    private Map<String, String> loadFilterChainDefinitions() {
         // 权限控制map.从数据库获取
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
         filterChainDefinitionMap.put("/", "anon");
@@ -63,13 +62,13 @@ public class ShiroService {
         filterChainDefinitionMap.put("/oauth/**", "anon");
         filterChainDefinitionMap.put(fileUploadProperties.getAccessPathPattern(), "anon");
         filterChainDefinitionMap.put(staticHtmlProperties.getAccessPathPattern(), "anon");
-       /* List<Menu> menuList = MenuService.selectAll(CoreConst.STATUS_VALID);
+        List<Menu> menuList = menuService.selectAll(CoreConst.STATUS_VALID);
         for (Menu menu : menuList) {
             if (StringUtils.isNotBlank(menu.getUrl()) && StringUtils.isNotBlank(menu.getPerms())) {
                 String perm = "perms[" + menu.getPerms() + ']';
                 filterChainDefinitionMap.put(menu.getUrl(), perm + ",kickOut");
             }
-        }*/
+        }
         filterChainDefinitionMap.put("/**", "user,kickOut");
         return filterChainDefinitionMap;
     }
